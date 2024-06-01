1b0e335ed34b38f2156b03e9fbd873eff62f0eb5
Greenxertz <Texbar07@gmail.com>
2024-06-02 01:46:04 +0200

    check a change

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 <body >
     <header></header>
-
+    <script src="assets/js/header-footer.js"></script>
     <section id="hero">
        <h4>Welcome to Tukkies Tools</h4>
        <h2>Find the tools you need for your practicals</h2>
