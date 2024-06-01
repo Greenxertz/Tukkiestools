5c70905ef58e85f57b5ee0543735695258927ba1
Greenxertz <Texbar07@gmail.com>
2024-06-02 01:56:04 +0200

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -1,4 +1,4 @@
 <?php
 
-$conn = mysqli_connect("localhost:3306","admin","yEnrgJq^)!f=","greenxer_php_tukkiestools")
+$conn = mysqli_connect("localhost","admin","yEnrgJq^)!f=","greenxer_php_tukkiestools")
     or die("couldnt connect"); 
