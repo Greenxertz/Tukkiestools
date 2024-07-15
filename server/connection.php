7cd43436d5850d08a802641ddae126ed62f08fe6
Marco Teixeira <148694559+Greenxertz@users.noreply.github.com>
2024-07-15 11:26:35 +0200

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -17,8 +17,3 @@ $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
-// Your code here...
-
-// Close the connection when done
-// mysqli_close($conn);
